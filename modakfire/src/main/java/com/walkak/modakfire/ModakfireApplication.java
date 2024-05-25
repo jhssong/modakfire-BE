cf5f67d171410b8d5bdc09fd2ca7308b1692ce59
lwj0831 <wjl0831@gmail.com>
2024-05-25 15:56:21 +0900

    initial setting DE

---
+++ modakfire/src/main/java/com/walkak/modakfire/ModakfireApplication.java
@@ -0,0 +1,13 @@
+package com.walkak.modakfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModakfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModakfireApplication.class, args);
+	}
+
+}
---
+++ modakfire/src/test/java/com/walkak/modakfire/ModakfireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walkak.modakfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModakfireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
